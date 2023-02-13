404367e6e54ab3d536736c48600916b420354642
Will Taylor <willtaylor2352@gmail.com>
2023-02-13 09:49:20 -0500

    Initial Commit
    Setting up the environment

---
+++ it4045c-group9/src/main/java/com/vidyaviewer/it4045cgroup9/It4045cGroup9Application.java
@@ -0,0 +1,13 @@
+package com.vidyaviewer.it4045cgroup9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It4045cGroup9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It4045cGroup9Application.class, args);
+	}
+
+}
---
+++ it4045c-group9/src/test/java/com/vidyaviewer/it4045cgroup9/It4045cGroup9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidyaviewer.it4045cgroup9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It4045cGroup9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
