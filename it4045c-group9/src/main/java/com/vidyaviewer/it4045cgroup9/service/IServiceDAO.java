f85e8df5ab82717171f280bb376408954030d7f4
Will Taylor <willtaylor2352@gmail.com>
2023-03-22 13:00:54 -0400

    Removed unnecessary imports

--- it4045c-group9/src/main/java/com/vidyaviewer/it4045cgroup9/service/IServiceDAO.java
+++ it4045c-group9/src/main/java/com/vidyaviewer/it4045cgroup9/service/IServiceDAO.java
@@ -2,9 +2,6 @@ package com.vidyaviewer.it4045cgroup9.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.vidyaviewer.it4045cgroup9.dao.IGameDAO;
 import com.vidyaviewer.it4045cgroup9.dto.GameDTO;
 
 public interface IServiceDAO {
--- it4045c-group9/src/main/java/com/vidyaviewer/it4045cgroup9/ui/VidyaViewerController.java
+++ it4045c-group9/src/main/java/com/vidyaviewer/it4045cgroup9/ui/VidyaViewerController.java
@@ -1,20 +1,15 @@
 package com.vidyaviewer.it4045cgroup9.ui;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.vidyaviewer.it4045cgroup9.dto.GameDTO;
 import com.vidyaviewer.it4045cgroup9.service.IServiceDAO;
-import com.vidyaviewer.it4045cgroup9.service.ServiceStub;
 
 @Controller
 public class VidyaViewerController {
