40cfb280c85780008491ccf27e3f2919c69defb5
Will Taylor <willtaylor2352@gmail.com>
2023-04-10 18:47:10 -0400

    Removal of user objects and profile page

--- it4045c-group9/src/main/java/com/vidyaviewer/it4045cgroup9/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.vidyaviewer.it4045cgroup9.dao;
-
-//implementation should be done here. Uncomment the following line to wire everything up
-//@Component
-public class UserDAO implements IUserDAO {
-
-}
